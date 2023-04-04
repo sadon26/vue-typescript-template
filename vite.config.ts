026a486d309d80f6509d2424ef013ec0c5a0ca7c
sadon26 <ashadayosamuel@gmail.com>
2023-04-04 17:03:37 +0100

    updated vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), EnvironmentPlugin([])],
+  plugins: [
+    vue(),
+    // Include your env variables in the array below using the name from the .env file by adding them as strings
+    // always starting with 'VITE-REACT-APP-${YOUR STRING}' e.g EnvironmentPlugin(['VITE-REACT-APP-API-BASE-URL'])
+    EnvironmentPlugin([])
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
